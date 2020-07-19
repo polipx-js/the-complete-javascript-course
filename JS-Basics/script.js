f94b040c19c8c1b2f657bf3b30fd7f1c7fac772a
Polipx <polipx.js@gmail.com>
2020-07-19 21:15:58 +0200

    variable mutation and type coercion

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -1,11 +1,17 @@
-var firstName = 'Patryk';
-console.log(firstName);
-var lastName = 'Olszewski';
-var age = 29;
-var fullAge = false;
-console.log(fullAge);
+var firstName = 'Polipx';
+var age = 28;
+//Type coercion
+console.log(firstName + ' ' + age);
 
-var job;
-console.log(job);
-job = 'Teacher';
-console.log('Teacher');
+var job, isMarried;
+job  = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old' + job + '. Is he married? ' + isMarried);
+
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' year old' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name');
+console.log(firstName + ' ' + lastName);
