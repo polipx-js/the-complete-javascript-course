86a3763b888c2983fb3cba2e85944be491b5f16e
Polipx <polipx.js@gmail.com>
2020-07-19 20:45:00 +0200

    variables and datatypes

--- JS-Basics/script.js
+++ JS-Basics/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!!!!");
+var firstName = 'Patryk';
+console.log(firstName);
+var lastName = 'Olszewski';
+var age = 29;
+var fullAge = false;
+console.log(fullAge);
+
+var job;
+console.log(job);
+job = 'Teacher';
+console.log('Teacher');
